8fc6e01f51830ae97345495dab2f4814c696dbd5
Vanessa Tan <vanessa_marie_tan@dlsu.edu.ph>
2023-03-28 21:44:39 +0800

    Add About me

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+],
+  theme: {
+    extend: {
+      colors: {
+        'palette-beige': '#EEE2DC',
+        'palette-beige-brown': '#EDC7B7',
+        'palette-gray': '#BAB2B5',
+        'palette-blue': {
+          500: '#123C69',
+          700: '#0E3054',
+          800: '#0D2A4A'
+        },
+        'palette-pink': '#AC3B61',
+      }
+    },
+    fontFamily:{
+      'sans': ['Roboto', 'sans-serif'],
+      'heading': ['OpenSans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
