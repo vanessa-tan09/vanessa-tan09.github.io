1bf9de106131f3d6a0ce99901459ac8300288c4d
Vanessa Tan <vanessa_marie_tan@dlsu.edu.ph>
2023-07-17 19:47:27 +0800

    Updated Projects and Contacts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -25,4 +27,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
